a3899f59d8695ca4a5db1cbaa8095e5b5ec5a7a7
skravche <sktechno92@gmail.com>
2019-03-23 16:29:05 +0200

    Updates

---
+++ precache-manifest.3ffbaf62517622b766c0da4209ea6e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mate-react-redux/005-web-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "129ff6c13290986a02f5",
+    "url": "/mate-react-redux/005-web-react/static/js/runtime~main.304b3d4b.js"
+  },
+  {
+    "revision": "91500a19d7ae7a372bb9",
+    "url": "/mate-react-redux/005-web-react/static/js/main.f874ab66.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/mate-react-redux/005-web-react/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "91500a19d7ae7a372bb9",
+    "url": "/mate-react-redux/005-web-react/static/css/main.c363957a.chunk.css"
+  },
+  {
+    "revision": "929e7f21dd7b763c5022b7d8473f5594",
+    "url": "/mate-react-redux/005-web-react/index.html"
+  }
+];
